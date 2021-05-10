3084709bda60c78a9613baccb9a013691e540eb9
Guilherme Santos <gui_t900@hotmail.com>
2021-05-10 20:32:19 -0300

    Docker finished

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -1,5 +1,6 @@
 package com.devsuperior.hrworker;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,6 +12,9 @@ public class HrWorkerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
+		
+		
+		
 	}
 
 }
